9c6d7027d9efb4c12cc25469abe80ddb14201b79
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-19 15:28:59 -0500

    Made win_check Function
    Made a function that checks whether O's or X's has won.  If you enter the board and an X or O the function should check every row, column, or diagonal to see if there are 3-in-a-row of X's or O's.

--- TicTacToe.py
+++ TicTacToe.py
@@ -28,8 +28,7 @@ def display_board(board):
     print('-----')
     print(board[7] +'|'+ board[8] +'|'+ board[9])
 
-
-board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+board = ['#','X','O','X','O','X','O','X','O','X']
 #display_board(board)
 
 
@@ -41,8 +40,27 @@ def place_marker(board, marker, position):
 
     board[position] = marker
 
-place_marker(board, 'X', 1)
+# place_marker(board, 'X', 1)
+# display_board(board)
+
+
+def win_check(board, mark):
+# Checks for 3 aligned X's or O's
+    return ((board[1] == mark and board[2] == mark and board[3] == mark) or
+    (board[4] == mark and board[5] == mark and board[6] == mark) or
+    (board[7] == mark and board[8] == mark and board[9] == mark) or
+    (board[1] == mark and board[4] == mark and board[7] == mark) or
+    (board[2] == mark and board[5] == mark and board[8] == mark) or
+    (board[3] == mark and board[6] == mark and board[9] == mark) or
+    (board[1] == mark and board[5] == mark and board[9] == mark) or
+    (board[3] == mark and board[5] == mark and board[7] == mark))
+
+
 display_board(board)
+win_check(board, 'X')
+
+
+
 
 
     # position = 'WRONG'
