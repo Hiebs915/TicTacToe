5d205773df5822b906ee2fe36003a356d64d6cf4
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-19 10:29:50 -0500

    Player 1 and 2 XO Assignment
    Created a function that asks if you want to be Player 1 or Player 2 and assign's X's to Player 1 and O's to Player 2 if you're input meets the function criteria.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,12 +1,47 @@
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+# board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-def display_board(board):
-    print(board[0] +'|'+ board[1] +'|'+ board[2])
-    print('-----')
-    print(board[3] +'|'+ board[4] +'|'+ board[5])
-    print('-----')
-    print(board[6] +'|'+ board[7] +'|'+ board[8])
+# def display_board(board):
+#     print(board[0] +'|'+ board[1] +'|'+ board[2])
+#     print('-----')
+#     print(board[3] +'|'+ board[4] +'|'+ board[5])
+#     print('-----')
+#     print(board[6] +'|'+ board[7] +'|'+ board[8])
 
-display_board(board)
+# display_board(board)
+
+
+
+
+def player_input():
+
+    player_choice = 'WRONG'
+    acceptable_digits = [1,2]
+    digit_choice = False
+
+    while player_choice.isdigit() == False or digit_choice == False:
+        player_choice = input('\nDo you want to be player 1 or 2?  Please enter "1" or "2": ')
+
+        # Two conditions to check: 1.) Is the input a digit? 2.) Is the input an acceptable digit(1 or 2)?
+        # Digit Check
+        if player_choice.isdigit() == False:
+            print("Sorry, that is not a digit")
+
+        # Acceptable Digit Check
+        if player_choice.isdigit() == True:
+            if int(player_choice) in acceptable_digits:
+                digit_choice = True
+                if int(player_choice) == 1:
+                    print("You are player 1 and are X's")
+                    player1 = "X"
+                if int(player_choice) == 2:
+                    print("You are player 2 and are O's")
+                    player2 = "O"
+            else:
+                print("Sorry, you did not enter a '1' or '2'")
+                digit_choice = False
+    return int(player_choice)
+
+
+player_input()
 
 
