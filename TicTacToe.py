022460571eb3c5690835b5f600962e525d1db1db
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-19 11:37:36 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -15,43 +15,44 @@ player_input()
 
 
 
-# board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-# def display_board(board):
-#     print(board[0] +'|'+ board[1] +'|'+ board[2])
-#     print('-----')
-#     print(board[3] +'|'+ board[4] +'|'+ board[5])
-#     print('-----')
-#     print(board[6] +'|'+ board[7] +'|'+ board[8])
-
-# display_board(board)
 
 
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+def display_board(board):
+    print(board[0] +'|'+ board[1] +'|'+ board[2])
+    print('-----')
+    print(board[3] +'|'+ board[4] +'|'+ board[5])
+    print('-----')
+    print(board[6] +'|'+ board[7] +'|'+ board[8])
+
+display_board(board)
 
 
 
 
-# def place_marker(board, marker, position):
-#     player_position = 'WRONG'
-#     game_board_digits = range(0,10)
-#     digit_choice = False
 
-#     while player_choice.isdigit() == False or digit_choice == False:
-#         player_position = input('\nPlease enter the positon you would like to place your marker on the board.  Please remember to enter a digit between 1-9': )
+def place_marker(board, marker, position):
+    player_position = 'WRONG'
+    game_board_digits = range(0,10)
+    digit_choice = False
 
-#         # Two conditions to check: 1.) Is the input a digit? 2.) Is the input an acceptable digit(1 or 2)?
-#         # Digit Check
-#         if player_position.isdigit() == False:
-#             print("Sorry, that is not a digit")
+    while player_choice.isdigit() == False or digit_choice == False:
+        player_position = input('\nPlease enter the positon you would like to place your marker on the board.  Please remember to enter a digit between 1-9': )
 
-#         # Acceptable Digit Check
-#         if player_position.isdigit() == True:
-#             if int(player_position) in game_board_digits:
-#                 digit_choice = True
-#             else:
-#                 print("Sorry, you did not enter a digit between 1-9")
-#                 digit_choice = False
+        # Two conditions to check: 1.) Is the input a digit? 2.) Is the input an acceptable digit(1 or 2)?
+        # Digit Check
+        if player_position.isdigit() == False:
+            print("Sorry, that is not a digit")
 
-#     return int(player_position)
+        # Acceptable Digit Check
+        if player_position.isdigit() == True:
+            if int(player_position) in game_board_digits:
+                digit_choice = True
+            else:
+                print("Sorry, you did not enter a digit between 1-9")
+                digit_choice = False
+
+    return int(player_position)
 
 
 
