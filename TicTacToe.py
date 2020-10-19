cc1b73995f535cfb06f518e4d45b0fdc6aaff8a4
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-19 10:28:14 -0500

    Game Board Creation and Layout
    Created Tic Tac Toe game board.  Board uses phone numpad from 1-9.  Example: location "3" is top-right position on the board.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,8 +1,12 @@
-game = ([0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0])
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def display_board(board):
+    print(board[0] +'|'+ board[1] +'|'+ board[2])
+    print('-----')
+    print(board[3] +'|'+ board[4] +'|'+ board[5])
+    print('-----')
+    print(board[6] +'|'+ board[7] +'|'+ board[8])
+
+display_board(board)
 
 
-print(game[0])
-print(game[1])
-print(game[2])
