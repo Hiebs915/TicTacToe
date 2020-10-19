dac318cf143e5a58581b3cf8dfa387d2cdff4639
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-19 08:29:43 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,21 +1,8 @@
-game = [[0, 0, 0],
+game = ([0, 0, 0],
         [0, 0, 0],
-        [0, 0, 0]]
+        [0, 0, 0])
 
 
-'''count = 0
-for row in game:
-    print(count, row)
-    count += 1'''
-
-def game_board():
-	print("   0  1  2")
-	for count, row in enumerate(game):
-   		print(count, row)
-
-game_board()
-
-game[0][1] = 2
-
-game_board()
-
+print(game[0])
+print(game[1])
+print(game[2])
