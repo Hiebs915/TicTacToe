9cb36576a3a4159d0c09f10dfcd9181c67e480c8
Hiebs915 <Matthew.Hiebing@gmail.com>
2020-10-20 19:45:11 -0500

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -78,7 +78,7 @@ def player_choice(board):
     position = 0
 
     while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
-        position = int(input(turn +' Choose your next position: (1-9) '))
+        position = int(input(turn +' Choose your position: (1-9) '))
 
     return position
 
